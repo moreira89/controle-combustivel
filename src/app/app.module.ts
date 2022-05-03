a66239d9c209422b28cff85242f5cfe6822c0cfd
rafael.moreira <rafael_moreira@outlook.com.br>
2022-05-02 21:08:33 -0300

    criacao dos componentes rodape e cabecalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
